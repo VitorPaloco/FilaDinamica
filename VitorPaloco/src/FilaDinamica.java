92d89d7eebaf77adf795dc850ac9db9c99153347
Vitor Paloco <127983254+VitorPaloco@users.noreply.github.com>
2024-05-22 19:45:50 -0300

    Update FilaDinamica.java

--- VitorPaloco/src/FilaDinamica.java
+++ VitorPaloco/src/FilaDinamica.java
@@ -14,7 +14,7 @@ public class FilaDinamica {
     }
 
     public void limpaTela() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println();
         }
     }
